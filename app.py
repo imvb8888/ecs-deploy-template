3548ef0b501653ed557acfd22582b26269240574
vbui <imvb8888@gmail.com>
2023-01-16 23:32:50 +0100

    with rds connection

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-# import os
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-# from dotenv import load_dotenv
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 # from secrets_manager import get_secret
 
@@ -23,44 +23,46 @@ def hello_world() -> str:
 #     f'{db_config["POSTGRES_DB"]}'
 # )
 
-# load_dotenv()
+load_dotenv()
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = (
-#     f'postgresql+psycopg2://{os.getenv("POSTGRES_USER")}:' +
-#     f'{os.getenv("POSTGRES_PW")}@' +
-#     f'{os.getenv("POSTGRES_HOST")}/' +
-#     f'{os.getenv("POSTGRES_DB")}'
-# )
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f'postgresql+psycopg2://{os.getenv("POSTGRES_USER")}:' +
+    f'{os.getenv("POSTGRES_PW")}@' +
+    f'{os.getenv("POSTGRES_HOST")}/' +
+    f'{os.getenv("POSTGRES_DB")}'
+)
 
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-# class Transaction(db.Model):
-#     __tablename__ = 'transactions'
+class Transaction(db.Model):
+    __tablename__ = 'transactions'
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.String())
-#     last_name = db.Column(db.String())
-#     amount = db.Column(db.Float)
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    amount = db.Column(db.Float)
 
-#     def __init__(self, first_name: str, last_name: str, amount: float) -> None:
-#         self.first_name = first_name
-#         self.last_name = last_name
-#         self.amount = amount
+    def __init__(self, first_name: str, last_name: str, amount: float) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.amount = amount
 
-#     def __repr__(self) -> str:
-#         return f'{self.first_name} {self.last_name} spent {self.amount}'
+    def __repr__(self) -> str:
+        return f'{self.first_name} {self.last_name} spent {self.amount}'
 
-# @app.route('/list_db')
-# def list_db() -> str:
-#     transactions = Transaction.query.all()
-#     if transactions:
-#         return '\n'.join([str(transaction) for transaction in transactions])
-#     else:
-#         return 'No transactions, yet!'
+
+@app.route('/list_db')
+def list_db() -> str:
+    transactions = Transaction.query.all()
+    if transactions:
+        return '\n'.join([str(transaction) for transaction in transactions])
+    else:
+        return 'No transactions, yet!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
